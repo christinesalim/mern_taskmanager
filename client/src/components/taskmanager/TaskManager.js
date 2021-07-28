f8f05eb5239de287047e593e7c7c892260919a2f
Christine Salim <christinesalim@gmail.com>
2021-07-28 16:22:44 -0700

    Removed warnings in TaskManager

--- client/src/components/taskmanager/TaskManager.js
+++ client/src/components/taskmanager/TaskManager.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { List } from 'semantic-ui-react';
 import Task from './Task';
-import { loadUser, fetchTasks } from '../../actions/index';
+import { fetchTasks } from '../../actions/index';
 import TaskCreateForm from './TaskCreateForm';
 import '../../styles/TaskManager.css';
 
@@ -17,41 +17,28 @@ const TaskManager = () => {
   const [tasks, setTasks] =  useState([]);
   const [displayedTasks, setDisplayedTasks] = useState([]);
   const prevSignedInStatus = useRef(false);
-       
-  //Get tasks if user just signed in
-  useEffect ( () => {
-    console.log("Task Manager useEffect prevSignedInStatus", prevSignedInStatus);
-    
-    //Sign in status changed from false to true so get this
-    //user's tasks
-    if (!prevSignedInStatus.current && isSignedIn){
-      console.log("TaskManager useEffect: user is NOW signed in! Fetching tasks");
 
-      //Get the user's tasks from the database
-      dispatch(fetchTasks());
-     
-    }
+  const fetchDatabaseTasks = useCallback( () => {
+    //Get the user's tasks from the database
+    dispatch(fetchTasks());
+  },[dispatch]);
 
-    //Save the current sign in status
-    prevSignedInStatus.current = isSignedIn;
-    console.log("Task Manager useEffect UPDATING prevSignedInStatus", prevSignedInStatus);
-  },[isSignedIn]);
-
-  //Update rendered tasks when the state store changes
-  useEffect (() => {
-    //Update our local copy of the tasks
-    setTasks(retrievedTasks);
-
-    //Update the list of tasks rendered based on the new list
-    
-    let tasksToDisplay = getTasksToRender (retrievedTasks)
-    console.log("TaskManager useEffect tasksToDisplay:", tasksToDisplay);
-    setDisplayedTasks(tasksToDisplay);
-    
-  }, [retrievedTasks]);
+  
 
   //Returns the tasks to render
-  const getTasksToRender = (tasks) => {
+  const getTasksToRender = useCallback( () => {
+    //Toggle the task with the id specified   
+    const toggleCompleted =  (id) => {
+      const updatedTasks = tasks.map(task => {
+        if (task.id ===id){
+          return { ...task, completed: !task.completed};
+        } else {
+          return task;
+        }
+      });
+      setTasks(updatedTasks);
+    }
+
     if(tasks){
       return tasks.map( task => {
         return (
@@ -65,19 +52,41 @@ const TaskManager = () => {
         );
       });
     }
-  }  
+  },[tasks] );
+       
+  //Get tasks if user just signed in
+  useEffect ( () => {
+    console.log("Task Manager useEffect prevSignedInStatus", prevSignedInStatus);
+    
+    //Sign in status changed from false to true so get this
+    //user's tasks
+    if (!prevSignedInStatus.current && isSignedIn){
+      console.log("TaskManager useEffect: user is NOW signed in! Fetching tasks");
+      fetchDatabaseTasks();    
+     
+    }
 
-  //Toggle the task with the id specified   
-  const toggleCompleted = (id) => {
-    const updatedTasks = tasks.map(task => {
-      if (task.id ===id){
-        return { ...task, completed: !task.completed};
-      } else {
-        return task;
-      }
-    });
-    setTasks(updatedTasks);
-  }
+    //Save the current sign in status
+    prevSignedInStatus.current = isSignedIn;
+    console.log("Task Manager useEffect UPDATING prevSignedInStatus", prevSignedInStatus);
+  },[isSignedIn, fetchDatabaseTasks]);
+
+  //Update rendered tasks when the state store changes
+  useEffect (() => {
+    //Update our local copy of the tasks
+    setTasks(retrievedTasks);
+
+    //Update the list of tasks rendered based on the new list
+    
+    let tasksToDisplay = getTasksToRender (retrievedTasks)
+    console.log("TaskManager useEffect tasksToDisplay:", tasksToDisplay);
+    setDisplayedTasks(tasksToDisplay);
+    
+  }, [retrievedTasks, getTasksToRender]);
+
+  
+
+  
 
   return (
     <div className="TaskManager">
